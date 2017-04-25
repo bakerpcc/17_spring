a122c8bbebc6f7c530d5b31ee7f5b5501d355316
bakerpcc <380513458@qq.com>
2017-04-26 00:02:36 +0800

    Create pagerank_graphx.py

---
+++ 5003/pagerank_graphx.py
@@ -0,0 +1,13 @@
+from graphframes import *
+from pyspark.sql.functions import *
+
+v=spark.read.csv('/Users/pc/spark-2.1.0-bin-hadoop2.7/vertices.csv',header=True,inferSchema=True)
+e=spark.read.csv('/Users/pc/spark-2.1.0-bin-hadoop2.7/edges.csv',header=True,inferSchema=True)
+
+g=GraphFrame(v,e)
+# g.vertices.show()
+# g.edges.show()
+# g.inDegrees.show()
+# g.outDegrees.show()
+
+results.vertices.select("id", "pagerank").orderBy("pagerank",ascending=False).show()
